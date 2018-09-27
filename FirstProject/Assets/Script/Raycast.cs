bcecb2526a197e22a7e5e3d92c537c750ad514d9
saranya-vatti <saranya.vatti@gmail.com>
2018-09-27 17:25:56 -0400

    Created projects
    Test - Plane, directional light, main camera, sphere and cube

---
+++ FirstProject/Assets/Script/Raycast.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // https://docs.unity3d.com/ScriptReference/RaycastHit-collider.html
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                if (hit.collider != null)
+                {
+
+                    Debug.Log("This is a computer");
+                }
+            }
+
+
+        }
+    }
+}
