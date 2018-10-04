1004f3ada40c936780ee7bc1af21672e0fda025e
Prahlad Misra <mprahlad95@gmail.com>
2018-10-04 12:37:11 -0400

    created whiteboard

---
+++ UnequalPay/Assets/WhiteBoardController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteBoardController : MonoBehaviour {
+
+    public GameObject uiObject;
+
+	// Use this for initialization
+	void Start () {
+        uiObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider player)
+    {
+        if(player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        uiObject.SetActive(false);
+    }
+}
