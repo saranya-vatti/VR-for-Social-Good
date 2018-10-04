c78765a0a4027282e09ccffcfc87330f0394bf92
saranya-vatti <saranya.vatti@gmail.com>
2018-10-04 03:34:48 -0400

    backup

---
+++ UnequalPay/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private string timeLeft;
+    private Text moneyEarned;
+    private double money;
+
+	// Use this for initialization
+	void Start () {
+        timeLeft = "08:00 hours";
+        money = 0.0;
+        moneyEarned.text = "Bank: $" + money.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
