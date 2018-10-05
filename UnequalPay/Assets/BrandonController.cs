1d1d75c2107c82c83b0d69b3839ab71f4f4123f4
Prahlad Misra <mprahlad95@gmail.com>
2018-10-05 06:27:40 -0400

    tasks done

---
+++ UnequalPay/Assets/BrandonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrandonController : MonoBehaviour {
+
+    public GameObject uiObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(8);
+        uiObject.SetActive(false);
+    }
+}
---
+++ UnequalPay/Assets/BrandonLaptopController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrandonLaptopController : MonoBehaviour {
+
+    private PlayerController playerControllerScript;
+    public GameObject uiObject;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("FirstPersonCharacter").GetComponent<PlayerController>();
+        uiObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+            playerControllerScript.UpdateStats(3, 600);
+        }
+
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        uiObject.SetActive(false);
+    }
+}
